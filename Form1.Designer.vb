6159c1f5750e3295851d52a0d0637c4a4de44b76
blitxs1226 <72100013+blitxs1226@users.noreply.github.com>
2022-11-03 01:34:44 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "com3"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(848, 31)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(8, 0, 8, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(281, 61)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "LogSensor"
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(96, 129)
+        Me.txtOutput.Margin = New System.Windows.Forms.Padding(8, 7, 8, 7)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(1929, 910)
+        Me.txtOutput.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(2242, 1160)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(8, 7, 8, 7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtOutput As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Threading
+Imports System.IO
+Imports System.Net
+Imports System.Net.Mail
+Imports System.Net.NetworkCredential
+Imports System.Reflection
+Imports System.Text
+Imports Microsoft.Win32
+Imports System.IO.Ports
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Button
+' thread-safe calling for thread_hide    
+
+
+Public Class Form1
+    Dim Contador As Integer = 0
+    Delegate Sub Change()
+    Private Declare Function InternetGetConnectedState Lib "wininet.dll" (ByRef lpdwFlags As Int32, ByVal dwReserved As Int32) As Boolean
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        SerialPort1.Close()
+        SerialPort1.PortName = "com3"
+        SerialPort1.BaudRate = "9600"
+        SerialPort1.DataBits = 8
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.Handshake = Handshake.None
+        SerialPort1.Encoding = System.Text.Encoding.Default
+        SerialPort1.Open()
+    End Sub
+
+    Private Sub dataReceived(ByVal sender As Object, ByVal e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Try
+            Dim mydata As String = ""
+
+            mydata = SerialPort1.ReadExisting()
+            If txtOutput.InvokeRequired Then
+                txtOutput.Invoke(DirectCast(Sub() txtOutput.Text &= "fuga detectada,Atender" + vbNewLine, MethodInvoker))
+                Contador = Contador + 1
+                If Contador > 3 Then
+                    Contador = 0
+                    Dim smtp As New SmtpClient
+                    Dim message As New MailMessage
+                    Dim username As String
+                    Dim password As String
+                    Dim destination As String
+                    Dim subject As String
+                    Dim lngFlags As Integer
+                    username = "infoarqui1226@gmail.com"
+                    password = "slelmmkyiscwbhnj"
+                    destination = "mjavier1226@gmail.com"
+                    subject = "HAY UNA FUGA, ATENDER PERO YA HIJUEPUTA"
+                    smtp.Host = "smtp.gmail.com"
+                    smtp.EnableSsl = True
+                    smtp.Port = 587
+                    smtp.Credentials = New Net.NetworkCredential(username, password)
+                    message.To.Add(destination)
+                    message.From = New MailAddress(username)
+                    message.Subject = subject
+                    message.Body = "Se han Registrado 3 Fugas , ATENDER URGENTEMENTE MIERDA"
+                    If InternetGetConnectedState(lngFlags, 0) Then
+                        ' checks if internet connection is available or not
+                        smtp.Send(message)
+                        MessageBox.Show("Alerta Enviada")
+                        ' CheckBox1.Checked = False
+                    Else txtOutput.Text = mydata + "" + vbNewLine + " net connection not available" + vbNewLine + ""
+                    End If
+                End If
+
+            Else
+                txtOutput.Text &= mydata
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If InStr(txtOutput.Text, "Exceeded") Then
+            'CheckBox1.Checked = True
+        End If
+    End Sub
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        'CheckBox1.Checked
+        If 1 = True Then
+            Dim smtp As New SmtpClient
+            Dim message As New MailMessage
+            Dim username As String
+            Dim password As String
+            Dim destination As String
+            Dim subject As String
+            Dim lngFlags As Integer
+            username = "infoarqui@gmail.com"
+            password = "mariojavier/01"
+            destination = "mjavier1226@gmail.com"
+            subject = "HAY UNA FUGA, ATENDER PERO YA HIJUEPUTA"
+            smtp.Host = "smtp.gmail.com"
+            smtp.EnableSsl = True
+            smtp.Port = 587
+            smtp.Credentials = New Net.NetworkCredential(username, password)
+            message.To.Add(destination)
+            message.From = New MailAddress(username)
+            message.Subject = subject
+            message.Body = txtOutput.Text
+            If InternetGetConnectedState(lngFlags, 0) Then
+                ' checks if internet connection is available or not
+                smtp.Send(message)
+                txtOutput.Text = ""
+                ' CheckBox1.Checked = False
+            Else txtOutput.Text = txtOutput.Text + "" + vbNewLine + " net connection not available" + vbNewLine + ""
+            End If
+        End If
+    End Sub
+End Class
